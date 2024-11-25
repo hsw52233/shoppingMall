494b13a2392df1772c44700cffc575d6a10f24a2
LJH0218 <krwnsgh@gmail.com>
2024-11-25 17:09:07 +0900

    241125/login page 완료

--- src/main/java/com/example/first/mapper/CustomerMapper.java
+++ src/main/java/com/example/first/mapper/CustomerMapper.java
@@ -2,7 +2,10 @@ package com.example.first.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.example.first.vo.Customer;
+
 @Mapper
 public interface CustomerMapper {
-
+	
+	Customer login(Customer customer);
 }
--- src/main/java/com/example/first/service/CustomerService.java
+++ src/main/java/com/example/first/service/CustomerService.java
@@ -1,8 +1,19 @@
 package com.example.first.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.first.mapper.CustomerMapper;
+import com.example.first.vo.Customer;
 
 @Service
+@Transactional
 public class CustomerService {
-
+	@Autowired CustomerMapper customerMapper;
+	
+	
+	public Customer login(Customer paramCustomer) {
+		return customerMapper.login(paramCustomer);
+	};
 }
