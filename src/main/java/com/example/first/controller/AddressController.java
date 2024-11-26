9c334cac3ace80d30a298ca11f1d1e91b1683b41
Lyun555 <askaskj3@gmail.com>
2024-11-26 16:50:02 +0900

    이동윤) addressList 기능 구현

--- src/main/java/com/example/first/controller/AddressController.java
+++ src/main/java/com/example/first/controller/AddressController.java
@@ -1,8 +1,27 @@
 package com.example.first.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.first.service.AddressService;
 
 @Controller
 public class AddressController {
 
+	@Autowired
+	private AddressService addressService;
+	
+	@GetMapping("/customer/addressList")
+	public String selectAddressList(Model model,@RequestParam String customerMail) {
+		
+		List<Map<String, Object>> addressList = addressService.getAddressList(customerMail);
+		model.addAttribute("addressList", addressList);
+		return "customer/addressList";
+	}
 }
