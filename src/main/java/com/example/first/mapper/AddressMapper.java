8e52f1a6cfb047fda169d6de298c94fafdcb5bda
Lyun555 <askaskj3@gmail.com>
2024-11-26 16:29:56 +0900

    이동윤) gitignore 변경

--- src/main/java/com/example/first/mapper/AddressMapper.java
+++ src/main/java/com/example/first/mapper/AddressMapper.java
@@ -1,8 +1,11 @@
 package com.example.first.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface AddressMapper {
-
+	List<Map<String,Object>> selectAddressList(String customerMail);
 }
--- src/main/java/com/example/first/service/AddressService.java
+++ src/main/java/com/example/first/service/AddressService.java
@@ -1,8 +1,13 @@
 package com.example.first.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.first.mapper.AddressMapper;
 
 @Service
 public class AddressService {
 
+	@Autowired
+	private AddressMapper addressMapper;
 }
