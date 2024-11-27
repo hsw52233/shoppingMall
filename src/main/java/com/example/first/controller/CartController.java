d72f0253743ad1438d24d1f6dd342b2451585170
Lyun555 <askaskj3@gmail.com>
2024-11-27 17:40:52 +0900

    이동윤) 장바구니 추가 구현

--- src/main/java/com/example/first/controller/CartController.java
+++ src/main/java/com/example/first/controller/CartController.java
@@ -7,10 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.first.service.CartService;
+import com.example.first.vo.Cart;
+import com.example.first.vo.Customer;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -19,11 +23,29 @@ public class CartController {
 
 	@Autowired
 	private CartService cartService;
-	
+
+	// Author : 이동윤
 	@GetMapping("/customer/cartList")
-	public String cartList(Model model,@RequestParam String customerMail) {
+	public String cartList(Model model, @RequestParam String customerMail) {
+
 		List<Map<String, Object>> cartList = cartService.getSelectCartList(customerMail);
-		model.addAttribute("cartList",cartList);
+		model.addAttribute("cartList", cartList);
 		return "customer/cartList";
+	}
+
+	// Author : 이동윤
+	@PostMapping("/customer/addCart")
+	public String addCart(HttpSession session, Cart cart) {	
+		String customerMail = ((Customer) (session).getAttribute("customerMail")).getCustomerMail();
+		cart.setCustomerMail(customerMail);
+		log.debug("cart : "+cart);
+		int row = cartService.getInsertCart(cart);
+		if(row == 1) {
+			return "redirect:/customer/cartList?customerMail="+cart.getCustomerMail();
+		}
+		
+		return "common/home";
+
+		
 	}
 }
--- src/main/java/com/example/first/mapper/CartMapper.java
+++ src/main/java/com/example/first/mapper/CartMapper.java
@@ -5,10 +5,14 @@ import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.example.first.vo.Cart;
+
 @Mapper
 public interface CartMapper {
 	
 	// Author : 이동윤
 	public List<Map<String,Object>> selectcartList(String customerMail);
+	
+	public int insertCart(Cart cart);
 
 }
