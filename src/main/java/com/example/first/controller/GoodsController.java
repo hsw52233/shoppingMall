a045a3f6238faace404580e3446b05cd20871aed
Lyun555 <askaskj3@gmail.com>
2024-11-27 11:24:26 +0900

    이동윤) 상품리스트, 상품 상세정보 추가

--- src/main/java/com/example/first/controller/GoodsController.java
+++ src/main/java/com/example/first/controller/GoodsController.java
@@ -1,5 +1,6 @@
 package com.example.first.controller;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.example.first.service.GoodsCategoryService;
 import com.example.first.service.GoodsFileService;
 import com.example.first.service.GoodsService;
+import com.example.first.vo.Page;
 
 @Controller
 public class GoodsController {
@@ -21,13 +23,28 @@ public class GoodsController {
 	private GoodsFileService goodsFileService;
 	@Autowired
 	private GoodsCategoryService goodsCategoryService;
-	
+
 	// Author : 이동윤 상품상세정보
 	@GetMapping("/common/goodsOne")
 	public String goodsOne(Model model, @RequestParam int goodsNo) {
 		Map<String, Object> goods = goodsService.getGoodsOne(goodsNo);
 		model.addAttribute("goods", goods);
-		return"common/goodsOne";
+		return "common/goodsOne";
+	}
+
+	// Author : 이동윤 상품리스트
+	@GetMapping("/common/goodsList")
+	public String getMethodName(Model model, Page page, @RequestParam(defaultValue = "0") int categoryNo) {
+		List<Map<String, Object>> goodsList = goodsService.getGoodsList(categoryNo, page);
+		int lastPage = goodsService.getLastPage(categoryNo);
+		model.addAttribute("categoryNo",categoryNo);
+		model.addAttribute("currentPage",page.getCurrentPage());
+		model.addAttribute("startPage",page.getStartPage());
+		model.addAttribute("numPerPage",page.getNumPerPage());
+		model.addAttribute("endPage",page.getEndPage());
+		model.addAttribute("lastPage",lastPage);
+		model.addAttribute("goodsList",goodsList);
+		return "common/goodsList";
 	}
 
 }
--- src/main/java/com/example/first/mapper/GoodsMapper.java
+++ src/main/java/com/example/first/mapper/GoodsMapper.java
@@ -1,12 +1,19 @@
 package com.example.first.mapper;
 
+import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
+
+import com.example.first.vo.Page;
 
 @Mapper
 public interface GoodsMapper {
 	
 	// 굿즈 상세정보
 	public Map<String,Object> selectGoodsOne(int goodsNo);
+
+	public List<Map<String, Object>> selectGoodsList(Map<String,Object> paramMap);
+	
+	public int goodsLastPage(int categoryNo);
 }
